49d2680359b5139a73bba26e98e62a36ff9a89af
Christophe Blanchet <christophe.blanchet@france-bioinformatique.fr>
2019-06-17 14:43:22 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script is executed on the virtual machine during the *Deployment* phase.
+# It is used to apply parameters specific to the current deployment.
+# It is executed secondly during a cloud deployement in IFB-Biosphere, after the *Installation* phase.
